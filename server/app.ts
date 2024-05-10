d96e017c4d8a1456e057f31bcb5bafe32bf13b7b
Umer Farooq <umer.farooq@dicetech.dev>
2024-05-10 17:25:52 +0500

    get and post request added on next js

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
 import Express from "express";
 import itemRouter from "./routes/item_routes";
+import cors from "cors";
 import { errorHandlerMiddleware } from "./controllers/errors";
 const app = Express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+app.use(cors());
 app.use(Express.json());
 app.use("/api/items", itemRouter);
 
