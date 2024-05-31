d73d4eba820ae2bc1be95d8073384f6be4f86eec
Umer Farooq <umer.farooq@dicetech.dev>
2024-05-31 16:06:41 +0500

    folder structure changed

---
+++ ecommerce/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "@/utils/saas/home.module.scss";
+import Navbar from "@/components/navbar/navbar";
+import MainContaint from "@/components/main/mainContaint";
+import Footer from "@/components/footer/footer";
+import TotalProducts from "./Total-Items/page";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <TotalProducts />
+    </main>
+  );
+}
---
+++ ecommerce/src/components/card/itemCard.tsx
@@ -0,0 +1,57 @@
+import styles from "@/utils/saas/itemCard.module.scss";
+
+interface ItemCardProps {
+  name: string;
+  price: number;
+  imageUrl: string;
+  brand: string;
+  stock: number;
+  updateBtn(): void;
+  deleteBtn(): void;
+  onclickBtn(): void;
+}
+
+const ItemCard: React.FC<ItemCardProps> = ({
+  name,
+  price,
+  imageUrl,
+  brand,
+  stock,
+  updateBtn,
+  deleteBtn,
+  onclickBtn,
+}) => {
+  const handleUpdateClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation(); // Stop event propagation
+    updateBtn(); // Call the updateBtn function
+  };
+
+  const handleDeleteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation(); // Stop event propagation
+    deleteBtn(); // Call the deleteBtn function
+  };
+
+  return (
+    <div className={styles.itemCard} onClick={onclickBtn}>
+      <img src={imageUrl} alt={name} className={styles.itemImage} />
+      <div className={styles.itemInfo}>
+        <h3 className={styles.itemName}>name:{name}</h3>
+        <h3 className={styles.itemBrand}>{brand}</h3>
+      </div>
+      <p className={styles.itemPrice}>${price.toFixed(2)}</p>
+      <div className={stock <= 0 ? styles.outOfStock : styles.inStock}>
+        Inn Stoock {stock}
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.seeMore} onClick={handleUpdateClick}>
+          See More
+        </button>
+        <button className={styles.delete} onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCard;
